01ddcf8495b8fc97fc1d53f9213453f51ea24cab
Akshat Toolaj Sinha <75127394+akshat1712@users.noreply.github.com>
2021-11-22 17:12:47 +0530

    updated_commenting

--- control.v
+++ control.v
@@ -2,16 +2,21 @@
 
 module control (clk,rst_button,game_mode1_button,game_mode2_button,led,buttons,an,seg);
 
+  // control module is the Top level module which calls other module and acts like a testbench for our game
+ // rst_buttons takes input from FPGA
 input clk, rst_button;
 input [15:0] buttons;
 
+  // game_mode1_button, game_mode2_button take input from FPGA
 input game_mode1_button,game_mode2_button;
 
+  // seg contains the cathode value for Seven segment display
+  // an tells which Anode we have to switch so that a segment display can grow
 output wire [6:0] seg;
 output wire [ 3:0] an;
 output wire [15:0] led; 
 
-
+// All the below variables stores / passes information from one module to another module 
 wire [6:0] score ;
 wire [6:0] max_score;
 wire [3:0] state;
@@ -29,7 +34,7 @@ wire game_mode1;
 wire game_mode2;
 
 
-
+// we are calling the specigic modules in a specific order to make our game work properly
 reset dut0(rst_button,rst, clk);  
 
 game_mode dut2( game_mode1_button,game_mode2_button,game_mode1,game_mode2,clk );
