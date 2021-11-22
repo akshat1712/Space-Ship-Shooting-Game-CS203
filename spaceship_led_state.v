09acaac0ba50673be7c641437f331973e856861e
Akshat Toolaj Sinha <75127394+akshat1712@users.noreply.github.com>
2021-11-22 18:09:59 +0530

    updated_commenting

--- spaceship_led_state.v
+++ spaceship_led_state.v
@@ -1,18 +1,35 @@
 module spaceship_LED_state(mode1, mode2, out_state, clk, rst,led_local);
+      // spaceship led_state outputs the state of the Leds and lights up the necessary Leds depends on the state
+      // we are always ligting up three consecutive LED
+      // State is the Leftmost leds that is lit on the FPGA, by using this one parameter , we can tell which led to light up 
+      // We have 2 game mode , one is a sequential one , in which leds will move from right to left at a jump of 2 leds
+      // another one in which led will move in a pseudo-random order determined below by algorithm
+      
+      // led_local outputs the current_state of the leds
+      
       input clk, rst, mode1, mode2;
       output [3:0] out_state;
       output reg[ 15:0] led_local;
       
-      reg [ 3:0 ] reg_state;      
+      // reg_state stores the current state of leds in a register
+      reg [ 3:0 ] reg_state;     
+      
+      // counter is used to slow down the Change of state of Leds , else we will see all led glow up as our eyes will not be able to work at the speed of the Clk
+      // hence we have use 5x10^7 as one cycle meaning after these many cycle , state will change and human eyes can perceive these changes  
       reg [31:0] counter;
+      // interger i for use in for loop
       integer i;
+      
+      // stay 1 and stay 2 will tells us which state we have to go , if mode1 and mode2 both are 0
       integer stay1=1,stay2=0;
       
-      reg [8:0] ran;
+      // ran and select is used in random_number generator 
+      reg [31:0] ran;
       integer select;
       
       always @ (posedge clk ) begin
       
+            // updating stay 1 and stay2 
             if( mode1==1'b1) begin
                 stay1=1;
                 stay2=0;
@@ -23,9 +40,13 @@ module spaceship_LED_state(mode1, mode2, out_state, clk, rst,led_local);
                 stay2=1;
             end
             
+            // if we have simple game mode
+            // then increase reg_state  by 2 until it becomes 14 , which made it changes it to 2 again
+            // we only do the above step if counter is 5x10^7
             if(stay1==1'b1)begin
                   stay1=1;
                   stay2=0;
+            // if rst is 1 , then reset the state to 2
                   if(rst==1'b1) begin
                       reg_state=4'b0010;
                       counter=0;
@@ -40,7 +61,8 @@ module spaceship_LED_state(mode1, mode2, out_state, clk, rst,led_local);
                    else
                         counter=counter+1; 
             end
-
+// if we are in random mode , then we add some prime number like 23,11,17,41 as prime number form a random distribution , 
+ // then we take modulus with 14 to get from 0 to 13 , and then add 2 to make it a valid stat i.e 2 to 15
             else begin
                   stay2=1;
                   stay1=0;
@@ -69,7 +91,7 @@ module spaceship_LED_state(mode1, mode2, out_state, clk, rst,led_local);
                         counter=counter+1;
             end
             
-            
+            // depending upon the state, we are ligting up the Leds
             for( i=0;i<16;i=i+1) begin
                   if( i==reg_state-2 || i==reg_state-1 || i==reg_state) 
                         led_local[i]=1;
@@ -77,7 +99,7 @@ module spaceship_LED_state(mode1, mode2, out_state, clk, rst,led_local);
                         led_local[i]=0;
             end
       end      
-      
+      // now are assigning reg_state to out_state for next iteration of this module.
       assign out_state=reg_state;
             
 
