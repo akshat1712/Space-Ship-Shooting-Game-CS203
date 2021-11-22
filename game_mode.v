3501204b704931d4474b02f9ee2b4746b8524254
Akshat Toolaj Sinha <75127394+akshat1712@users.noreply.github.com>
2021-11-22 17:17:27 +0530

    updated_commenting

--- game_mode.v
+++ game_mode.v
@@ -2,6 +2,10 @@
 
 
 module game_mode( game_mode1_button_local , game_mode2_button_local ,game_mode1,game_mode2,clk);
+    // similiar to rst, we are also doing the same to avoid debouncing of our inputs
+    // game_mode1 stores if mode 1 is to be played 
+    // game_mode2 stores if mode2 is to be played
+    // It is assumed  that player does not press both game mode to avoid confusion which game mode we have to play
     output reg game_mode1;
     output reg game_mode2;
     input game_mode1_button_local , game_mode2_button_local , clk;
