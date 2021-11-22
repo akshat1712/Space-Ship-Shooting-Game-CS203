c6b702bb167e7ff77ff2f5beef9536e3ad1a9658
Akshat Toolaj Sinha <75127394+akshat1712@users.noreply.github.com>
2021-11-22 17:03:47 +0530

    updated_commenting

--- rst.v
+++ rst.v
@@ -1,7 +1,8 @@
 module reset (button_rst_local,rst,clk);
       input button_rst_local,clk;
       output reg rst;
-
+// button_rst_local get the inputs from the control module
+// rst is the value that depicts if button is pressed or not and takes care of debouncing problem .
       always @( posedge clk) begin
             
       if( button_rst_local==1'b1)
